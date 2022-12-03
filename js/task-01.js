501bbc543218182e3fece3299da2e99848dcb31e
EvgenyVeliki <108056667+EvgenyVeliki@users.noreply.github.com>
2022-12-03 16:58:56 +0200

    new Task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,20 @@
 const categories = document.querySelector('#categories').children.length
 console.dir(`Number of categories: ${categories}`)
 
-const animals = document.querySelector('.js-list_animals').children.length
-console.dir(`Category: Animals
-Elements: ${animals}`)
+const firstCategories = document.querySelector('.js-list_animals').children.length
+const firstCategorie = document.querySelector('.js-firstCategoriesName').textContent
 
-const products = document.querySelector('.js-list_products').children.length
+console.dir(`Category: ${firstCategorie}
+Elements: ${firstCategories}`)
+
+const secondCategories = document.querySelector('.js-list_products').children.length
+const secondCategorie = document.querySelector('.js-secondCategoriesName').textContent
 console.dir(`
-Category: Products
-Elements: ${products}`)
+Category: ${secondCategorie}
+Elements: ${secondCategories}`)
 
 
-const technologies = document.querySelector('.js-list_technologies').children.length
-console.dir(`Category: Technologies
-Elements: ${technologies}`)
+const thirdCategories = document.querySelector('.js-list_technologies').children.length
+const thirdCategorie = document.querySelector('.js-thirdCategoriesName').textContent
+console.dir(`Category: ${thirdCategorie}
+Elements: ${thirdCategories}`)
