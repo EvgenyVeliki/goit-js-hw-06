3b451af80325b21bf052bf930d37444b45679c99
EvgenyVeliki <108056667+EvgenyVeliki@users.noreply.github.com>
2022-12-03 16:12:48 +0200

    Task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,16 @@
-const list = document.querySelector("js-item")
-console.log(list)
+const categories = document.querySelector('#categories').children.length
+console.dir(`Number of categories: ${categories}`)
+
+const animals = document.querySelector('.js-list_animals').children.length
+console.dir(`Category: Animals
+Elements: ${animals}`)
+
+const products = document.querySelector('.js-list_products').children.length
+console.dir(`
+Category: Products
+Elements: ${products}`)
+
+
+const technologies = document.querySelector('.js-list_technologies').children.length
+console.dir(`Category: Technologies
+Elements: ${technologies}`)
